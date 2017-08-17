e88072eb0687488581f23c2c2e03cc0652e44b95
Branka Jaceva <brankajaceva@gmail.com>
2017-08-17 16:50:29 +0200

    changed menu

---
+++ Styles/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.jumbotron').addClass('zoom');
+});
+
+$(document).ready(function() {
+$("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $(".wrapper").toggleClass("active");
+  });
+});
