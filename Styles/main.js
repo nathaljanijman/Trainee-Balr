9126b7e0e30e748ba9adcb92f57b1bd4f335c387
Nathalja Nijman <nathaljanijman@hotmail.com>
2017-08-31 09:25:13 +0200

    fade in fucntion in supporting container

--- Styles/main.js
+++ Styles/main.js
@@ -8,3 +8,20 @@ $("#menu-toggle").click(function(e) {
       $(".wrapper, .sidebar-wrapper, body").toggleClass("active");
   });
 });
+
+$(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.hideme').each( function(i){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $('.balr').animate({'opacity':'1', 'margin-top':'0'},1000);
+                $('.courses').delay(500).animate({'opacity':'1', 'margin-top':'0'},1000);
+                $('.trainees').delay(1000).animate({'opacity':'1', 'margin-top':'0'},1000);
+            }
+        });
+    });
+});
