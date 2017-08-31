0ae8cea3e15fc7ce07efd4c887494920d3a37e2e
Daan den Boer <daandenboer@MacBook-van-Daan.local>
2017-08-31 11:38:56 +0200

    added icons and links

--- Styles/main.js
+++ Styles/main.js
@@ -18,9 +18,9 @@ $(document).ready(function() {
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
-                $('.balr').animate({'opacity':'1', 'margin-top':'0'},1000);
-                $('.courses').delay(500).animate({'opacity':'1', 'margin-top':'0'},1000);
-                $('.trainees').delay(1000).animate({'opacity':'1', 'margin-top':'0'},1000);
+                $('.balr').animate({'opacity':'1', 'margin-top':'0', 'margin-bottom':'30px'},1000);
+                $('.courses').delay(500).animate({'opacity':'1', 'margin-top':'0', 'margin-bottom':'30px'},1000);
+                $('.github').delay(1000).animate({'opacity':'1', 'margin-top':'0', 'margin-bottom':'30px'},1000);
             }
         });
     });
