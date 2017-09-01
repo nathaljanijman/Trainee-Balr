d4afde19e687ede3ca3c80318195fc8028d9c3b5
Daan den Boer <daandenboer@MacBook-van-Daan.local>
2017-09-01 10:43:17 +0200

    setup uglifier

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp       = require('gulp');
 var lessImport = require('gulp-less-import');
 var less       = require('gulp-less');
-var uglify     = require('gulp-uglify');
+var uglifycss  = require('gulp-uglifycss');
 
 gulp.task('default', function() {
   gulp.src('./styles/css/*.less')
     .pipe(lessImport('trainee.less'))
     .pipe(less())
+    .pipe(uglifycss())
     .pipe(gulp.dest('./styles/css'));
 });
